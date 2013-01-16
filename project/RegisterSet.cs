033690cc319568b9c586e21cb3736323ade34a63
bsv798 <bsv798@gmail.com>
2013-01-16 08:36:21 +0700

    add RegisterSet class

---
+++ project/RegisterSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mipsDasm
+{
+    public abstract class RegisterSet
+    {
+        protected Register[] arr;
+
+        public RegisterSet()
+        {
+
+        }
+
+        public Register getRegister(int idx)
+        {
+            return arr[idx];
+        }
+    }
+}
