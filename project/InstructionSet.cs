015e5f7bb627ba208587dc83c066be0e88b18d9b
bsv798 <bsv798@gmail.com>
2013-01-16 08:44:24 +0700

    add InstructionSet class

---
+++ project/InstructionSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mipsDasm
+{
+    public abstract class InstructionSet
+    {
+        public InstructionSet()
+        {
+
+        }
+
+        public abstract Instruction getInstruction(int binary);
+    }
+}
