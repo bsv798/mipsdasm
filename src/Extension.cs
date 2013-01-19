0c57c48abd026e3a5200ff661d83a2e0f39d0fd3
bsv798 <bsv798@gmail.com>
2013-01-19 12:43:37 +0700

    add extension class

---
+++ src/Extension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mipsDasm
+{
+    public static class Extension
+    {
+        public static int getIntFromBits(this int val, int startBit, int bitCount)
+        {
+            return (val << (32 - (startBit + bitCount))) >> (32 - bitCount);
+        }
+
+        public static int getUIntFromBits(this int val, int startBit, int bitCount)
+        {
+            return (val >> startBit) & ((1 << bitCount) - 1);
+        }
+
+        public static void assertEquals(this int actual, int expected)
+        {
+            if (actual != expected)
+                throw new ArgumentException(string.Format("Assert equals failed: {0} != {1}.", actual, expected));
+        }
+
+        public static void assertEqualsZero(this int actual)
+        {
+            assertEquals(actual, 0);
+        }
+    }
+}
