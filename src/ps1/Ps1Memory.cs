e4a6a708ddb3c3eca44adeb2289fc203538b8e2f
bsv798 <bsv798@gmail.com>
2013-11-03 22:23:24 +0700

    add ps1 memory class

---
+++ src/ps1/Ps1Memory.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mipsDasm.ps1
+{
+    public class Ps1Memory : Memory
+    {
+        public Ps1Memory()
+            : base(2 * 1024 * 1024)
+        {
+
+        }
+    }
+}
+
